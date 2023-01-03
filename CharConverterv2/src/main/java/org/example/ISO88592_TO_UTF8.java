c4cef8d24077d13f61a15c1b94a59941011ba19a
radkornel <56034300+radkornel@users.noreply.github.com>
2023-01-03 15:04:19 +0100

    Add files via upload

---
+++ CharConverterv2/src/main/java/org/example/ISO88592_TO_UTF8.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import io.confluent.ksql.function.udf.Udf;
+import io.confluent.ksql.function.udf.UdfDescription;
+import io.confluent.ksql.function.udf.UdfParameter;
+import io.confluent.ksql.util.BytesUtils;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+
+@UdfDescription(name = "ISO88592_TO_UTF8", description = "Change the given ISO-8859-2 encoded Bytes to a UTF8 String")
+public class ISO88592_TO_UTF8 {
+    @Udf(description = "Converts the given BYTES value to STRING in the specified encoding.")
+    public String iso88592_to_utf8(@UdfParameter(description = "The bytes value to convert.") final ByteBuffer value) throws UnsupportedEncodingException {
+        byte[] bytes_iso88592 = BytesUtils.getByteArray(value);
+        byte[] utf8 = new String(bytes_iso88592, "ISO-8859-2").getBytes("UTF-8");
+        return new String(utf8, "UTF-8");
+    }
+}
